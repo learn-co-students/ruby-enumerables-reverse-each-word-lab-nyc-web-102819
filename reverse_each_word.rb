c5b15eee06b8ee5450161e7e6cce955aeb2979f6
Hector Polanco <hdp2112@columbia.edu>
2019-10-21 21:55:29 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(string)
+  array = string.split(" ")
+  reversed_array = []
+  array.each do |element|
+    reversed_array << element.reverse
+  end
+p reversed_array.join(" ")
+p collect_reversed = (array.collect { |word| word.reverse }).join(" ")
+end
